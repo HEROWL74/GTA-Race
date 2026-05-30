2d0b94a188626ab2b4c2e95cdc437eaa7273cc2d
HEROWL <hiroakitogawa74@gmail.com>
2026-05-30 16:59:42 +0900

    Add Project

---
+++ Source/GTARace/Core/SharedData.h
@@ -0,0 +1,30 @@
+#pragma  once
+
+#include "CoreMinimal.h"
+#include "Runtime/MassEntity/Public/MassEntityTypes.h"
+
+// ---------------------------
+// Fragments (個別のデータ)
+// ---------------------------
+
+// 位置、移動速度を所持するデータ
+USTRUCT(BlueprintType)
+struct FMyDataFragment : public FMassFragment
+{
+	GENERATED_BODY()
+	
+	UPROPERTY()
+	FVector Velocity = FVector::ZeroVector;
+	
+	UPROPERTY()
+	float Speed = 100.0f;
+};
+
+// ---------------------------
+// Tags (識別子)
+// ---------------------------
+USTRUCT()
+struct FCrowdTag : public FMassFragment
+{
+	GENERATED_BODY()
+};
