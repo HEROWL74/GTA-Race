30e62969132c6d6654a8a4d8ca00412288899f4a
HEROWL <hiroakitogawa74@gmail.com>
2026-06-30 21:08:49 +0900

    feat(PlayerCharacter): add player input action and input context mappings to control playercharacter

---
+++ Source/GTARace/Public/InputDataConfig.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InputDataConfig.generated.h"
+
+class UInputAction;
+
+UCLASS()
+class GTARACE_API UInputDataConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* Move;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* Look;
+};
