fdcad6f6d4a20d5600360f282244a2bbc7237a05
HEROWL <hiroakitogawa74@gmail.com>
2026-07-13 13:11:47 +0900

    Feature/police actor spawn (#4)
    * ci(main.yml): upgrade actions checkout version
    
    * chore: organize project folder structure
    
    * feat(PoliceAI): add police nav mesh ai to create police template

---
+++ Source/GTARace/Private/PoliceController.cpp
@@ -0,0 +1,19 @@
+// PoliceController.cpp
+
+#include "PoliceController.h"
+
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+APoliceController::APoliceController()
+{
+
+}
+
+void APoliceController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+}
+
+
