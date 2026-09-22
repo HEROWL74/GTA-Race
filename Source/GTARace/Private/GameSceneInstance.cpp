340e177f81f095f7477852d601b39dc5adc14721
HEROWL <hiroakitogawa74@gmail.com>
2026-09-22 10:23:30 +0900

    feat(Title Sequence): add title intro sequence to create title animation fix(WheeledVehicle Blueprint): Modified to send the Pawn value to the server

--- Source/GTARace/Private/GameSceneInstance.cpp
+++ Source/GTARace/Private/GameSceneInstance.cpp
@@ -2,68 +2,92 @@
 #include "OnlineSubsystem.h"
 #include "OnlineSessionSettings.h"
 #include "OnlineSubsystemUtils.h"
-#include "Online/OnlineSessionNames.h"
+#include "Kismet/GameplayStatics.h"
 
 UGameSceneInstance::UGameSceneInstance()
 {
-	
 }
 
 void UGameSceneInstance::Init()
 {
-	if (IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld()))
-	{
-		SessionInterface = Subsystem->GetSessionInterface();
-		if (SessionInterface.IsValid())
-		{
-			// Bind Delegates Here
-			SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UGameSceneInstance::OnCreateSessionComplete);
-			SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UGameSceneInstance::OnFindSessionComplete);
-		}
-	}
+	Super::Init();
+	
+    if (IOnlineSubsystem* Subsystem = Online::GetSubsystem(GetWorld()))
+    {
+       SessionInterface = Subsystem->GetSessionInterface();
+       if (SessionInterface.IsValid())
+       {
+          SessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UGameSceneInstance::OnCreateSessionComplete);
+          SessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this, &UGameSceneInstance::OnFindSessionComplete);
+          SessionInterface->OnJoinSessionCompleteDelegates.AddUObject(this, &UGameSceneInstance::OnJoinSessionComplete);
+       }
+    }
 }
 
-void UGameSceneInstance::OnCreateSessionComplete(FName SessionName, bool Succeeded)
+void UGameSceneInstance::OnCreateSessionComplete(FName ServerName, bool Succeeded)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnCreateSessionComplete, Succeeded: %d"), Succeeded);
-	if (Succeeded)
-	{
-		GetWorld()->ServerTravel("/Game/Game/Maps/Levels/GameScene?listen");
-	}
+    UE_LOG(LogTemp, Warning, TEXT("OnCreateSessionComplete, Succeeded: %d"), Succeeded);
+    if (Succeeded)
+    {
+       GetWorld()->ServerTravel("/Game/Game/Maps/Levels/GameScene?listen");
+    }
 }
 
 void UGameSceneInstance::OnFindSessionComplete(bool Succeeded)
 {
-	UE_LOG(LogTemp, Warning, TEXT("OnFindSessionComplete, Succeeded: %d"), Succeeded);
-	if (Succeeded)
-	{
-		TArray<FOnlineSessionSearchResult> SearchResults = SessionSearch->SearchResults;
-		UE_LOG(LogTemp, Warning, TEXT("SearchResults, Server Count: %d"), SearchResults.Num());
-	}
+    UE_LOG(LogTemp, Warning, TEXT("OnFindSessionComplete, Succeeded: %d"), Succeeded);
+    if (Succeeded)
+    {
+       TArray<FOnlineSessionSearchResult> SearchResults = SessionSearch->SearchResults;
+       
+       UE_LOG(LogTemp, Warning, TEXT("SearchResults, Server Count: %d"), SearchResults.Num());
+       
+       if (SearchResults.Num() > 0)
+       {
+          UE_LOG(LogTemp, Warning, TEXT("Joining Server"));
+          SessionInterface->JoinSession(0, FName("MySession"), SearchResults[0]);
+       }
+    }
+}
+
+void UGameSceneInstance::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+    UE_LOG(LogTemp, Warning, TEXT("OnJoinSessionComplete, SessionName: %s"), *SessionName.ToString());
+    if (APlayerController* PController = UGameplayStatics::GetPlayerController(GetWorld(), 0))
+    {
+       FString JoinAddress = "";
+       if (SessionInterface.IsValid() && SessionInterface->GetResolvedConnectString(SessionName, JoinAddress))
+       {
+          if (!JoinAddress.IsEmpty())
+          {
+             PController->ClientTravel(JoinAddress, TRAVEL_Relative);
+          }
+       }
+    }
 }
 
 void UGameSceneInstance::CreateServer()
 {
-	UE_LOG(LogTemp, Warning, TEXT("CreateServer"));
-	FOnlineSessionSettings SessionSettings;
-	SessionSettings.bAllowJoinInProgress = true;
-	SessionSettings.bIsDedicated = false;
-	SessionSettings.bIsLANMatch = true;
-	SessionSettings.bShouldAdvertise = true;
-	SessionSettings.bUsesPresence = true;
-	SessionSettings.bUseLobbiesIfAvailable = true;
-	SessionSettings.NumPublicConnections = 5;
-	
-	SessionInterface->CreateSession(0, FName("My Session"), SessionSettings);
+    UE_LOG(LogTemp, Warning, TEXT("CreateServer"));
+    if (!SessionInterface.IsValid()) return;
+
+    FOnlineSessionSettings SessionSettings;
+    SessionSettings.bAllowJoinInProgress = true;
+    SessionSettings.bIsDedicated = false;
+    SessionSettings.bIsLANMatch = true;       // LANモード有効
+    SessionSettings.bShouldAdvertise = true;    // 検索にヒットさせる
+    SessionSettings.NumPublicConnections = 5;
+    
+    SessionInterface->CreateSession(0, FName("MySession"), SessionSettings);
 }
 
 void UGameSceneInstance::JoinServer()
 {
-	SessionSearch = MakeShareable(new FOnlineSessionSearch());
-	SessionSearch->bIsLanQuery = true; // IS LAN
-	SessionSearch->MaxSearchResults = 10000;
-	
-	SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
-	SessionSearch->QuerySettings.Set(SEARCH_KEYWORDS, true, EOnlineComparisonOp::Equals);
-}
+    UE_LOG(LogTemp, Warning, TEXT("JoinServer"));
+    if (!SessionInterface.IsValid()) return;
 
+    SessionSearch = MakeShareable(new FOnlineSessionSearch());
+    SessionSearch->bIsLanQuery = true;
+    SessionSearch->MaxSearchResults = 100;
+    SessionInterface->FindSessions(0, SessionSearch.ToSharedRef());
+}
--- Source/GTARace/Public/GameSceneInstance.h
+++ Source/GTARace/Public/GameSceneInstance.h
@@ -20,8 +20,9 @@ protected:
 	TSharedPtr<FOnlineSessionSearch> SessionSearch;
 	virtual void Init() override;
 	
-	virtual void OnCreateSessionComplete(FName SessionName, bool Succeeded);
+	virtual void OnCreateSessionComplete(FName ServerName, bool Succeeded);
 	virtual void OnFindSessionComplete(bool Succeeded);
+	virtual void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
 	
 	UFUNCTION(BlueprintCallable)
 	void CreateServer();
